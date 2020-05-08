d1f4f3574faf78e1930dfa62747672acfd10eef6
Roman Pishchalnikov <roman.pishchalnikov@gmail.com>
2020-05-08 22:57:38 +0700

    Add ToDo model

---
+++ src/test/kotlin/models/ToDo.kt
@@ -0,0 +1,19 @@
+package models
+
+import com.fasterxml.jackson.annotation.JsonProperty
+
+const val ToDosURL = "/todos"
+
+data class ToDo(
+        @JsonProperty("userId")
+        val userID: Int,
+
+        @JsonProperty("id")
+        val id: Int,
+
+        @JsonProperty("title")
+        val title: String,
+
+        @JsonProperty("completed")
+        val completed: Boolean
+)
