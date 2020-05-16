d8e210c868c54594a1510eec076b88bd50218886
Roman Pishchalnikov <roman.pishchalnikov@gmail.com>
2020-05-16 16:30:07 +0700

    Add getToDoByIDTest

--- src/test/kotlin/tests/TestToDoAPI.kt
+++ src/test/kotlin/tests/TestToDoAPI.kt
@@ -2,12 +2,28 @@ package tests
 
 import base.BaseCase
 import io.restassured.RestAssured.given
+import models.ToDo
 import models.ToDosURL
 import org.hamcrest.CoreMatchers.equalTo
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
 import org.junit.jupiter.params.provider.ValueSource
+import java.util.stream.Stream
 
 class TestToDoAPI : BaseCase() {
+
+    companion object {
+        @JvmStatic
+        private fun todoProvider(): Stream<ToDo> = Stream.of(
+                ToDo(
+                        userID = 1,
+                        id = 1,
+                        title = "delectus aut autem",
+                        completed = false
+                )
+        )
+    }
 
     @ParameterizedTest
     @ValueSource(ints = [200])
@@ -18,5 +34,20 @@ class TestToDoAPI : BaseCase() {
         .then()
                 .statusCode(200)
                 .body("$.size()", equalTo(size))
+    }
+
+    @ParameterizedTest
+    @MethodSource("todoProvider")
+    fun getToDoByIDTest(expectedToDo: ToDo) {
+        val todo = given()
+            .`when`()
+                    .get("$ToDosURL/${expectedToDo.id}")
+            .then()
+                    .statusCode(200)
+                    .extract()
+                    .body()
+                    .jsonPath()
+                    .getObject(".", ToDo::class.java)
+        assertEquals(expectedToDo, todo)
     }
 }
