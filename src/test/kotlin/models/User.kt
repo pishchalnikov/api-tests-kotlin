282de548acd012c89e49ec5970ada30d15511c18
Roman Pishchalnikov <roman.pishchalnikov@gmail.com>
2020-05-20 22:16:35 +0700

    Add User model

---
+++ src/test/kotlin/models/User.kt
@@ -0,0 +1,64 @@
+package models
+
+import com.fasterxml.jackson.annotation.JsonProperty
+
+const val usersURL = "/users"
+
+data class User(
+        @JsonProperty("id")
+        val id: Int,
+
+        @JsonProperty("name")
+        val name: String,
+
+        @JsonProperty("email")
+        val email: String,
+
+        @JsonProperty("address")
+        val address: Address?,
+
+        @JsonProperty("phone")
+        val phone: String,
+
+        @JsonProperty("website")
+        val website: String,
+
+        @JsonProperty("company")
+        val company: Company?
+) {
+    data class Address(
+            @JsonProperty("street")
+            val street: String,
+
+            @JsonProperty("suite")
+            val suite: String,
+
+            @JsonProperty("city")
+            val city: String,
+
+            @JsonProperty("zipcode")
+            val zipcode: String,
+
+            @JsonProperty("geo")
+            val geo: Geo?
+    ) {
+        data class Geo(
+                @JsonProperty("lat")
+                val lat: String,
+
+                @JsonProperty("lng")
+                val lng: String
+        )
+    }
+
+    data class Company(
+            @JsonProperty("name")
+            val name: String,
+
+            @JsonProperty("catchPhrase")
+            val catchPhrase: String,
+
+            @JsonProperty("bs")
+            val bs: String
+    )
+}
