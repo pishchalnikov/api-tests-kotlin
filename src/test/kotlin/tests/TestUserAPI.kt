55ed324f6d6eb98cb34bd07f23fd821297e17840
Roman Pishchalnikov <roman.pishchalnikov@gmail.com>
2020-05-21 20:56:15 +0700

    Add sizeUserTest

--- src/test/kotlin/tests/TestUserAPI.kt
+++ src/test/kotlin/tests/TestUserAPI.kt
@@ -1,8 +1,22 @@
 package tests
 
 import base.BaseCase
-
+import io.restassured.RestAssured.given
+import models.usersURL
+import org.hamcrest.CoreMatchers.equalTo
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 class TestUserAPI : BaseCase() {
 
+    @ParameterizedTest
+    @ValueSource(ints = [10])
+    fun sizeUserTest(size: Int) {
+        given()
+        .`when`()
+                .get(usersURL)
+        .then()
+                .statusCode(200)
+                .body("$.size()", equalTo(size))
+    }
 }
